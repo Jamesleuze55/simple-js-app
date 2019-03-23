061e2753a45ce7304fc75bb151df9258a0a72c93
Jim Leuze <jamesleuze55@gmail.com>
2019-03-23 12:13:09 -0400

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -10,7 +10,7 @@ document.write('<h1>Pokemon</h1>');
 var h ='';
 for (var a = 0; a<repository.length; a++) {
   if (repository[a].height > 16.9); {
-    h = "-Wow That's Big!!";
+    h = " That's Big!!";
   }
 
   document.write('<div>' + repository[a].name + '(height: ' + repository[a].height +') ' + h +' </div>');}
