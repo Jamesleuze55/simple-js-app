a0e8179401e2a1f9a5910616ac3bfa4592494814
Jim Leuze <jamesleuze55@gmail.com>
2019-03-25 20:19:20 -0400

    was working on trying to trouble shoot my code, copied William's as it worked and changes mine to notes to compare.

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,4 @@
-var repository = [
+/* var repository = [
   {name: 'Weedle', height: 3, types: ['bugs', 'poision']},
   {name: 'Bulbasaur', height: 7, types: ['grass', 'poision']},
   {name: 'Gigalith', height: 17, types: ['rock']},
@@ -7,9 +7,30 @@ var repository = [
 console.log(repository);
 
 document.write('<h1>Pokemon</h1>');
-
 var h ='';
 for (var a = 0; a<repository.length; a++) {
-  if (repository[a].height >=16.9); {
-  document.write('<div>' + repository[a].name + '(height: ' + repository[a].height +') ' + h +' </div>');}
-} else if ()
+ if (repository[a].height >16.9) {
+    h = "-Wow That's Big!!";
+  document.write('<div>' + repository[a].name + '(height: ' +  repository[a].height + ') ' + h +' </div>');
+}
+*/
+
+
+
+var repository = [
+ {name: 'Weedle', height: 3, types: ['bugs', 'poision']},
+ {name: 'Bulbasaur', height: 7, types: ['grass', 'poision']},
+ {name: 'Gigalith', height: 17, types: ['rock']},
+];
+
+console.log(repository);
+
+document.write('<h1>Pokemon</h1>');
+var h ='';
+for (var a = 0; a<repository.length; a++) {
+ if (repository[a].height > 16.9) {
+   h = " That's Big!!";
+ }
+
+ document.write('<div>' + repository[a].name + '(height: ' +                     repository[a].height +') ' + h +' </div>');
+}
