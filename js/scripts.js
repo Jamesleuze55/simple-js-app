98f0b8bb9a512829a35c1216fcc55c73dfc86361
Jim Leuze <jamesleuze55@gmail.com>
2019-04-02 19:38:10 -0400

    Wrapped code in an IIFE to avoid accidentally acessing the global state

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,5 @@
-/* var repository = [
+(function(){
+ var repository = [
   {name: 'Weedle', height: 3, types: ['bugs', 'poision']},
   {name: 'Bulbasaur', height: 7, types: ['grass', 'poision']},
   {name: 'Gigalith', height: 17, types: ['rock']},
@@ -11,12 +12,9 @@ var h ='';
 for (var a = 0; a<repository.length; a++) {
  if (repository[a].height >16.9) {
     h = "-Wow That's Big!!";
-  }
+}
   document.write('<div>' + repository[a].name + '(height: ' +  repository[a].height + ') ' + h +' </div>');
 }
-*/
-
-
 
 var repository = [
  {name: 'Weedle', height: 3, types: ['bugs', 'poision']},
@@ -34,4 +32,5 @@ for (var a = 0; a<repository.length; a++) {
  }
 
  document.write('<div>' + repository[a].name + '(height: ' + repository[a].height +') ' + h +' </div>');
-}
+  }
+}) ();
