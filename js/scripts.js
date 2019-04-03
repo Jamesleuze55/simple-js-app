b41b33424c502203f42b7ee4ca03b8dd2e26cd12
Jim Leuze <jamesleuze55@gmail.com>
2019-04-02 20:11:39 -0400

    Commit test

--- js/scripts.js
+++ js/scripts.js
@@ -1,36 +1,46 @@
-(function(){
- var repository = [
-  {name: 'Weedle', height: 3, types: ['bugs', 'poision']},
-  {name: 'Bulbasaur', height: 7, types: ['grass', 'poision']},
-  {name: 'Gigalith', height: 17, types: ['rock']},
-];
 
-console.log(repository);
+var pokemonRepository = (function(){
+  var repository = [
+    {name: 'Weedle', height: 3, types: ['bugs', 'poision']},
+    {name: 'Bulbasaur', height: 7, types: ['grass', 'poision']},
+    {name: 'Gigalith', height: 17, types: ['rock']},
+  ];
 
-document.write('<h1>Pokemon</h1>');
-var h ='';
-for (var a = 0; a<repository.length; a++) {
- if (repository[a].height >16.9) {
-    h = "-Wow That's Big!!";
-}
-  document.write('<div>' + repository[a].name + '(height: ' +  repository[a].height + ') ' + h +' </div>');
-}
+  console.log(repository);
 
-var repository = [
- {name: 'Weedle', height: 3, types: ['bugs', 'poision']},
- {name: 'Bulbasaur', height: 7, types: ['grass', 'poision']},
- {name: 'Gigalith', height: 17, types: ['rock']},
-];
-
-console.log(repository);
-
-document.write('<h1>Pokemon</h1>');
-var h ='';
-for (var a = 0; a<repository.length; a++) {
- if (repository[a].height > 16.9) {
-   h = "-Wow That's Big!!";
- }
-
- document.write('<div>' + repository[a].name + '(height: ' + repository[a].height +') ' + h +' </div>');
+  document.write('<h1>Pokemon</h1>');
+  var h ='';
+  for (var a = 0; a<repository.length; a++) {
+    if (repository[a].height >16.9) {
+      h = "-Wow That's Big!!";
+    }
+    document.write('<div>' + repository[a].name + '(height: ' +  repository[a].height + ') ' + h +' </div>');
   }
-}) ();
+
+  var repository = [
+    {name: 'Weedle', height: 3, types: ['bugs', 'poision']},
+    {name: 'Bulbasaur', height: 7, types: ['grass', 'poision']},
+    {name: 'Gigalith', height: 17, types: ['rock']},
+  ];
+
+  console.log(repository);
+
+  document.write('<h1>Pokemon</h1>');
+  var h ='';
+  for (var a = 0; a<repository.length; a++) {
+    if (repository[a].height > 16.9) {
+      h = "-Wow That's Big!!";
+    }
+
+    document.write('<div>' + repository[a].name + '(height: ' + repository[a].height +') ' + h +' </div>');
+  }
+  function getAll(){
+    return repository
+  }
+  function add(item){
+    return repository.push()
+  }
+  return {
+    add: add,
+    getAll: getAll};
+  }) ();
