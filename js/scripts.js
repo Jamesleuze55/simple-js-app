40f72d3c60dc5d1b266d690ca9dc3108e1d88701
Jim Leuze <jamesleuze55@gmail.com>
2019-04-03 20:05:59 -0400

    added the array.forEach loop

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,4 @@
-
 var pokemonRepository = (function(){
-  var repository = [
-    {name: 'Weedle', height: 3, types: ['bugs', 'poision']},
-    {name: 'Bulbasaur', height: 7, types: ['grass', 'poision']},
-    {name: 'Gigalith', height: 17, types: ['rock']},
-  ];
-
-  console.log(repository);
-
-  document.write('<h1>Pokemon</h1>');
-  var h ='';
-  for (var a = 0; a<repository.length; a++) {
-    if (repository[a].height >16.9) {
-      h = "-Wow That's Big!!";
-    }
-    document.write('<div>' + repository[a].name + '(height: ' +  repository[a].height + ') ' + h +' </div>');
-  }
-
   var repository = [
     {name: 'Weedle', height: 3, types: ['bugs', 'poision']},
     {name: 'Bulbasaur', height: 7, types: ['grass', 'poision']},
@@ -38,9 +20,15 @@ var pokemonRepository = (function(){
     return repository
   }
   function add(item){
-    return repository.push()
+    return repository.push(item)
   }
   return {
     add: add,
     getAll: getAll};
   }) ();
+
+  pokemonRepository.getAll();
+
+  pokemonRepository.getAll().forEach(function(pokemon){
+    console.log(pokemon.name);
+  });
